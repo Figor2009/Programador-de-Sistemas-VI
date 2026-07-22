349cd6dd99c152a1d53335e03f851faa44df7881
Figor2009 <igor.antonio.almeida@escola.pr.gov.br>
2026-07-22 13:45:05 -0300

    Feito

--- TrabalhosStruct/EXStruct3.c
+++ TrabalhosStruct/EXStruct3.c
@@ -1,1 +1,36 @@
-//Criar uma struct pessoa que contem uma struct endereco, ppreencher os dados e depois utilizando ponteiros imprimir na tela
+#include <stdio.h>
+#include <string.h>
+
+struct Endereco
+{
+    char rua[50];
+    int numero;
+};
+
+struct Pessoa
+{
+    char nome[50];
+    int idade;
+    struct Endereco end;
+};
+
+int main()
+{   
+    struct Pessoa pes;
+    struct Endereco end;
+    
+    printf("Insira Nome e idade: \n");
+    scanf("%[^\n]", pes.nome);
+    scanf("%d", &pes.idade);
+    printf("Insira Rua e número da residência: \n");
+    scanf("%s", end.rua);
+    scanf("%d", &end.numero);
+    
+    printf("Nome: %s\n", pes.nome);
+    printf("Idade: %d\n", pes.idade);
+    printf("ENDEREÇO\n");
+    printf("Rua: %s\n", end.rua);
+    printf("Número: %d\n", end.numero);
+    
+    return (0);
+}
